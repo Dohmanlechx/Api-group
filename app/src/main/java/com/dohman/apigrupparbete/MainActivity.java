beef25f5c130e08499b94a482827f90292b1aba2
David Öhman <dohman_92@hotmail.com>
2018-05-03 11:28:38 +0200

    Update

--- app/src/main/java/com/dohman/apigrupparbete/MainActivity.java
+++ app/src/main/java/com/dohman/apigrupparbete/MainActivity.java
@@ -1,13 +1,25 @@
 package com.dohman.apigrupparbete;
 
+import android.graphics.Movie;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,37 @@ public class MainActivity extends AppCompatActivity {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                // Getting inputstream from connection, that is response which we get from server
+                InputStream inputStream = urlConnection.getInputStream();
+                // Reading the response
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String s = bufferedReader.readLine();
+                bufferedReader.close();
+                // Returning the response message to onPostExcute
+                return s;
+            } catch (IOException e) {
+                Log.e(TAG, "Error: ", e);
+            }
+            return null;
         }
+        // This method runs on UIThread and it will execute when doInBackground is completed
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            JSONObject json = null;
+
+            try {
+                // Parent JSON Object. JSON Objects starts at { and ends at }
+                json = new JSONObject(s);
+
+                ArrayList<Movie> movie = new ArrayList<>();
+
+                // JSON Array of parent JSON Object. JSON Arrays starts at [ and ends at ]
+                JSONArray jsonArray = json.getJSONArray()
+            }
+        }
+
     }
 }
